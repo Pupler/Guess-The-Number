57c81bb2a724a0dde113016d0b276030e74d7cd3
Pupler <misha.reak45@gmail.com>
2025-11-27 11:52:48 +0100

    Implement basic game code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
+int randomNumber() {
+    random_device rdNum;
+    mt19937 generate(rdNum());
+    uniform_int_distribution<int> range(1, 100);
+    int randomNum = range(generate);
+    return range(generate);
+}
+
 int main() {
+    int number;
+    int randomNum = randomNumber();
+
     cout << "====GUESS THE FUCKING NUMBER====" << endl;
-    
+    cout << "\nI chose random number from 1 to 100. Try to guess it: ";
+    cin >> number;
+
+    if (number != randomNum) {
+        cout << "LOSER" << endl;
+        cout << "The number was: " << randomNum << endl;
+    } else {
+        cout << "WINNER!" << endl;
+    }
 }
